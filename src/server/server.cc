456cea648f9a8191179177642638f264664c166a
Mathias Erik Nord <mathieno@stud.ntnu.no>
2026-03-11 20:59:05 +0100

    Fixed bug in the server.

--- src/server/server.cc
+++ src/server/server.cc
@@ -2,12 +2,20 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int server(int listenPort) {
-  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+int server(const int listenPort) {
+  const int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (serverSocket < 0) {
     perror("Failed to create socket");
+    return -1;
+  }
+
+  if (constexpr int opt = 1; setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    perror("Failed to set socket options");
+    close(serverSocket);
     return -1;
   }
 
@@ -17,23 +25,44 @@ int server(int listenPort) {
   serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   errorCheck(bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr),
-                   sizeof(serverAddr)), "Failed to bind serverSocket.");
+                 sizeof(serverAddr)), "Failed to bind serverSocket.");
 
   errorCheck(listen(serverSocket, 5), "Failed to listen to serverSocket.");
 
-  int clientSocket = accept(serverSocket, nullptr, nullptr);
+  cout << "Server listening on port " << listenPort << "..." << endl;
+
+  const int clientSocket = accept(serverSocket, nullptr, nullptr);
+  if (clientSocket < 0) {
+    perror("Failed to accept connection");
+    close(serverSocket);
+    return -1;
+  }
+
+  cout << "Client connected!" << endl;
+
+  char buffer[1024] = {};
   while (true) {
-    if (clientSocket < 0) {
-      perror("Failed to accept connection");
-      continue;
+    memset(buffer, 0, sizeof(buffer));
+    const int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+    if (bytesRead < 0) {
+      perror("Error receiving data");
+      break;
+    }
+    if (bytesRead == 0) {
+      cout << "Client disconnected" << endl;
+      break;
     }
 
-    char buf[1024] = {};
-    recv(clientSocket, buf, sizeof(buf), 0);
-    cout << "Message from client: " << buf << endl;
+    cout << "Message from client: " << buffer << endl;
 
-    close(clientSocket);
+    string response = "Message received: " + string(buffer);
+    send(clientSocket, response.c_str(), response.length(), 0);
   }
+
+  close(clientSocket);
+  close(serverSocket);
+  return 0;
 }
 
 int main() {
